2501e3864c342b280fa16efcd21ed1fef75b1f3f
OriónZYX <cristian_cris85@outlook.com>
2025-03-26 08:24:29 -0600

    Main

--- app/src/main/java/com/example/miruta/MainActivity.kt
+++ app/src/main/java/com/example/miruta/MainActivity.kt
@@ -3,45 +3,36 @@ package com.example.miruta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.miruta.ui.theme.MiRutaTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MiRutaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyComposeScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyComposeScreen() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text = "¡Hola, Jetpack Compose!")
+        Button(onClick = { }) {
+            Text("Presionar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MiRutaTheme {
-        Greeting("Android")
-    }
+fun PreviewScreen() {
+    MyComposeScreen()
 }
